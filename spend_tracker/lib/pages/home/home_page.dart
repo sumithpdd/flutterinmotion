ddb57fed0c4255e983bd35898e20a500a99a66d8
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-05-29 23:19:04 +0200

    Box decoration with amount

--- spend_tracker/lib/pages/home/home_page.dart
+++ spend_tracker/lib/pages/home/home_page.dart
@@ -4,6 +4,7 @@ import 'package:spend_tracker/pages/home/widgets/custom_text.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var amount = "1,203.00";
     return Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.menu),
@@ -16,28 +17,37 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            const Text(
-              'One',style: TextStyle(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            height: 100,
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            child: Text(
+              '\$$amount',
+              style: TextStyle(
+                color: Colors.white,
                 fontSize: 40,
-                color: Colors.green,
-                fontWeight: FontWeight.bold,
-                decoration: TextDecoration.underline
               ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.green,
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.green, Colors.white54, Colors.blueGrey,],
+                stops: [0.85, 0.95, 1.0],
               ),
-               const CustomText(
-              text:'Two',
+              boxShadow: [
+                BoxShadow(color: Colors.grey, offset: Offset(4, 4)),
+              ],
+              borderRadius: BorderRadius.all(
+                Radius.circular(5),
               ),
-               const Text(
-              'Three',
-              ),
-              Image.network('https://gravatar.com/avatar/1735fca48036474bb3e30abc295c1067',
-              height:200)
-           
-          ],
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
