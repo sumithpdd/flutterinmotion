226f74850c2a69f1ca87aac1980fd950e392de3f
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-05-30 21:09:40 +0200

    layout with rows and columns

--- spend_tracker/lib/pages/home/home_page.dart
+++ spend_tracker/lib/pages/home/home_page.dart
@@ -19,39 +19,119 @@ class HomePage extends StatelessWidget {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
+          _TotalBudget(amount: amount),
           Container(
-            height: 100,
-            margin: EdgeInsets.all(10),
-            alignment: Alignment.center,
-            child: Text(
-              '\$$amount',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 40,
-              ),
-            ),
-            decoration: BoxDecoration(
-              color: Colors.green,
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Colors.green,
-                  Colors.white54,
-                  Colors.blueGrey,
-                ],
-                stops: [0.85, 0.95, 1.0],
-              ),
-              boxShadow: [
-                BoxShadow(color: Colors.grey, offset: Offset(4, 4),),
+            padding: EdgeInsets.only(bottom: 10),
+            height: MediaQuery.of(context).size.height - 200,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _BarLine(100,Colors.red,506),
+                _BarLine(400,Colors.green
+                ,1706),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 400,
+                      width: 100,
+                      color: Colors.green,
+                    ),
+                    Text(
+                      'Deposit',
+                    ),
+                    Text(
+                      '\$1709',
+                    ),
+                  ],
+                ),
               ],
-              borderRadius: BorderRadius.all(
-                Radius.circular(5),
-              ),
             ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _BarLine extends StatelessWidget {
+  const _BarLine(
+    this.height,
+    this.color,
+    this.amount, {
+    Key key,
+  }) : super(key: key);
+  final double height;
+  final Color color;
+  final int amount;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          height: height,
+          width: 100,
+          color: color,
+        ),
+        Text(
+          'Withdraw',
+        ),
+        Text(
+          '\$$amount',
+        ),
+      ],
+    );
+  }
+}
+
+class _TotalBudget extends StatelessWidget {
+  const _TotalBudget({
+    Key key,
+    @required this.amount,
+  }) : super(key: key);
+
+  final String amount;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      margin: EdgeInsets.all(10),
+      alignment: Alignment.center,
+      child: Text(
+        '\$$amount',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 40,
+        ),
+      ),
+      decoration: BoxDecoration(
+        color: Colors.green,
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Colors.green,
+            Colors.white54,
+            Colors.blueGrey,
+          ],
+          stops: [0.85, 0.95, 1.0],
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            offset: Offset(4, 4),
           ),
         ],
+        borderRadius: BorderRadius.all(
+          Radius.circular(5),
+        ),
       ),
     );
   }
